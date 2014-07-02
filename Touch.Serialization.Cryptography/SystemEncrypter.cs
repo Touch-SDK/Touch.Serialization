e039431deb14f86e8378fba173f6fdafb091ab54
Artem Abashev <artem@abashev.com>
2014-07-02 20:25:31 +0400

    Added built-in .NET cryptography wrappers.

---
+++ Touch.Serialization.Cryptography/SystemEncrypter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Touch.Serialization
+{
+    /// <summary>
+    /// Default .NET encrypter.
+    /// </summary>
+    public sealed class SystemEncrypter : IEncrypter
+    {
+        #region .ctor
+        public SystemEncrypter()
+        {
+            _hashAlgorithm = null;
+        }
+
+        public SystemEncrypter(string hashAlgorithm)
+        {
+            if (string.IsNullOrWhiteSpace(hashAlgorithm)) throw new ArgumentNullException("hashAlgorithm");
+            _hashAlgorithm = hashAlgorithm;
+        }
+        #endregion
+
+        private readonly string _hashAlgorithm;
+
+        public byte[] Crypt(byte[] data, byte[] salt)
+        {
+            var dst = new byte[salt.Length + data.Length];
+
+            Buffer.BlockCopy(salt, 0, dst, 0, salt.Length);
+            Buffer.BlockCopy(data, 0, dst, salt.Length, data.Length);
+
+            var algorithm = _hashAlgorithm != null ? HashAlgorithm.Create(_hashAlgorithm) : HashAlgorithm.Create();
+            return algorithm.ComputeHash(dst);
+        }
+
+        public byte[] Crypt(byte[] data)
+        {
+            var algorithm = _hashAlgorithm != null ? HashAlgorithm.Create(_hashAlgorithm) : HashAlgorithm.Create();
+            return algorithm.ComputeHash(data);
+        }
+    }
+}
---
+++ Touch.Serialization.Cryptography/SystemHashGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Touch.Serialization
+{
+    /// <summary>
+    /// Default .NET hash generator.
+    /// </summary>
+    public sealed class SystemHashGenerator : IHashGenerator
+    {
+        #region .ctor
+        public SystemHashGenerator()
+        {
+            _hashAlgorithm = null;
+        }
+
+        public SystemHashGenerator(string hashAlgorithm)
+        {
+            if (string.IsNullOrWhiteSpace(hashAlgorithm)) throw new ArgumentNullException("hashAlgorithm");
+            _hashAlgorithm = hashAlgorithm;
+        } 
+        #endregion
+
+        private readonly string _hashAlgorithm;
+
+        public string Generate(byte[] data)
+        {
+            var algorithm = _hashAlgorithm != null ? HashAlgorithm.Create(_hashAlgorithm) : HashAlgorithm.Create();
+            var inArray = algorithm.ComputeHash(data);
+
+            return Convert.ToBase64String(inArray);
+        }
+    }
+}
