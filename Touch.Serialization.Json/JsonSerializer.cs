84570d61690c97dfcc0695a55a705dd8fd7b409a
Artem Abashev <artem@abashev.com>
2014-07-02 19:38:52 +0400

    Initial import.

---
+++ Touch.Serialization.Json/JsonSerializer.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using System;
+
+namespace Touch.Serialization
+{
+    /// <summary>
+    /// JSON serializer.
+    /// </summary>
+    sealed public class JsonSerializer : ISerializer
+    {
+        #region ISerializer implementation
+        public void Serialize<T>(T obj, Stream output)
+            where T : class, new()
+        {
+            var data = Serialize(obj);
+            var bytes = Encoding.UTF8.GetBytes(data);
+
+            var length = bytes.Length;
+            var i = 0;
+
+            while (i < length)
+            {
+                var count = Math.Min(bytes.Length - i, 32);
+                output.Write(bytes, i, count);
+                i += 32;
+            }
+
+            output.Flush();
+        }
+
+        public T Deserialize<T>(Stream stream)
+            where T : class, new()
+        {
+            var bytes = new byte[stream.Length];
+            var i = 0;
+
+            while (i < bytes.Length)
+            {
+                var count = Math.Min(bytes.Length - i, 32);
+                i += stream.Read(bytes, i, count);
+            }
+
+            var data = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+
+            return Deserialize<T>(data);
+        }
+
+        public string Serialize<T>(T obj)
+            where T : class, new()
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public T Deserialize<T>(string data)
+            where T : class, new()
+        {
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+        #endregion
+    }
+}
